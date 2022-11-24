1241f8af8f3e9e4ad612ef3e709e8f4b9bd298b7
Justin <Justin.Swatloski@gmail.com>
2022-11-24 14:09:22 -0600

    Working SQL connection with /champions

--- src/main/java/org/rpgApp/RPGApp/RPGController.java
+++ src/main/java/org/rpgApp/RPGApp/RPGController.java
@@ -1,10 +1,15 @@
 package org.rpgApp.RPGApp;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import org.rpgApp.RPGApp.ChampRepo;
+import org.rpgApp.RPGApp.Champion;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,10 +18,31 @@ import java.util.List;
 public class RPGController {
 
 
+    @Autowired
+    ChampRepo champRepo;
+
+
 
     @GetMapping("/home")
     public String homePage(Model model){
     return "home";
+    }
+
+    @GetMapping("/champions")
+    public ResponseEntity<List<Champion>> getChamps(){
+        try{
+            List<Champion> champList = new ArrayList<Champion>();
+
+            champRepo.findAll().forEach(champList::add);
+
+            if(champList.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(champList, HttpStatus.OK);
+        } catch (Exception e){
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @GetMapping("/game")
