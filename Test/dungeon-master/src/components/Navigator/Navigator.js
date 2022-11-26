dd075e35c84982671ae1aa779efc3ba1678db730
Justin <Justin.Swatloski@gmail.com>
2022-11-25 23:10:52 -0600

    created a navigator wrapper

---
+++ Test/dungeon-master/src/components/Navigator/Navigator.js
@@ -0,0 +1,27 @@
+import MonsterMenu from '../Monsters/MonsterMenu';
+import CampaignMenu from '../Campaigns/CampaignMenu';
+import ArenaMenu from '../Arena/ArenaMenu';
+import ChampionMenu from '../Champions/ChampionMenu';
+import DiceMenu from '../Dice/DiceMenu';
+import CampaignCreator from '../Campaigns/CampaignCreator';
+import {Route, Routes} from 'react-router-dom';
+import OptionsMenu from "../Options/OptionsMenu";
+
+
+const Navigator = () => {
+
+    return (
+        <Routes>
+        <Route path="/" element={<OptionsMenu />} />
+
+        <Route path="/Monsters" element={<MonsterMenu />} />
+  <Route path="/Champions" element={<ChampionMenu />} />
+  <Route path="/Dice" element={<DiceMenu />} />
+  <Route path="/Arena" element={<ArenaMenu  />} />
+  <Route path="/Campaigns/" element={<CampaignMenu  /> } />
+  <Route path="/Campaigns/Create" element={<CampaignCreator  />} />
+  </Routes>
+    )
+};
+
+export default Navigator;
