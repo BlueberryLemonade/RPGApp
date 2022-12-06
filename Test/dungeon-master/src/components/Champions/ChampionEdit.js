905140bc625df9e3c61bef25ac280c0291a91d6d
Justin <Justin.Swatloski@gmail.com>
2022-12-05 20:27:08 -0600

    Proper navigation on champ edit submission

--- Test/dungeon-master/src/components/Champions/ChampionEdit.js
+++ Test/dungeon-master/src/components/Champions/ChampionEdit.js
@@ -1,13 +1,16 @@
 import './ChampionForm.css';
-import {  useState} from 'react';
+import { useState } from 'react';
 import axios from 'axios';
 import { Link, useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
+
+import { useNavigate } from 'react-router-dom';
+
 const ChampionEdit = (props) => {
 
 
     const [enteredName, setEnteredName] = useState("");
-    
+
     //Currently using let so I can turn the number into a string later for verification purposes, 
     //need to find a more elegant solution
     let [enteredHP, setEnteredHP] = useState("");
@@ -18,32 +21,33 @@ const ChampionEdit = (props) => {
     const location = useLocation();
     const { id } = location.state;
 
-   
     const [loading, setLoading] = useState(false);
+
+    const navigate = useNavigate();
 
     useEffect(() => {
         setLoading(true);
-    
-        async function fetchData() {
-        const request = await axios.get('http://localhost:8080/api/champions/' + id);
-        setLoading(false);
-        setEnteredName(request.data.name);
-        setEnteredHP(request.data.hp);
-       
-        return request;
-    } 
-    fetchData();
-    }, []);
-    
-    
-if(loading){
-    return <p>Loading...</p>;
-}
-   
- 
-   
 
-    
+        async function fetchData() {
+            const request = await axios.get('http://localhost:8080/api/champions/' + id);
+            setLoading(false);
+            setEnteredName(request.data.name);
+            setEnteredHP(request.data.hp);
+
+            return request;
+        }
+        fetchData();
+    }, []);
+
+
+    if (loading) {
+        return <p>Loading...</p>;
+    }
+
+
+
+
+
 
     const nameChangeHandler = (event) => {
         const holder = event.target.value;
@@ -93,61 +97,60 @@ if(loading){
         const updatedChampion = {
             name: enteredName,
             hp: enteredHP,
-           
+
         };
 
-        
 
-            axios({
-                method: 'put',
-                          url: 'http://localhost:8080/api/champions/' + id,
-                     data: updatedChampion
-                       })
 
-        setEnteredName("");
-        setNameIsValid(false);
+        axios({
+            method: 'put',
+            url: 'http://localhost:8080/api/champions/' + id,
+            data: updatedChampion
+        })
 
-        setEnteredHP("");
-        setStrengthIsValid(false);
-        
-       
-           
-                
-        
-               
+        navigate("/Champions");
+
+
+
+
+
+
+
+
+
 
     };
 
 
     return (
         <div>
-        <form onSubmit={verifier}>
-            <div>
-                <h2>Champion Creation</h2>
-                <div >
-                    <label className='labelText'>Champion Name: </label>
-                    <input
-                        className={`nameField ${!nameIsValid ? 'invalid' : ''}`}
-                        type='text'
-                        value={enteredName}
-                        onChange={nameChangeHandler}
-                    ></input>
-                </div>
+            <form onSubmit={verifier}>
                 <div>
-                    <label className='labelText'>HP: </label>
-                    <input
-                        type='number'
-                        className={`strengthField ${!strengthIsValid ? 'invalid' : ''}`}
-                        min="0"
-                        max="9999"
-                        value={enteredHP}
-                        onChange={HPChangeHandler}></input>
+                    <h2>Champion Creation</h2>
+                    <div >
+                        <label className='labelText'>Champion Name: </label>
+                        <input
+                            className={`nameField ${!nameIsValid ? 'invalid' : ''}`}
+                            type='text'
+                            value={enteredName}
+                            onChange={nameChangeHandler}
+                        ></input>
+                    </div>
+                    <div>
+                        <label className='labelText'>HP: </label>
+                        <input
+                            type='number'
+                            className={`strengthField ${!strengthIsValid ? 'invalid' : ''}`}
+                            min="0"
+                            max="9999"
+                            value={enteredHP}
+                            onChange={HPChangeHandler}></input>
+                    </div>
                 </div>
-            </div>
-            <button type='submit' >Confirm</button>
-        </form>
-                  <Link to="/Champions/"><button>Back</button></Link>
-                  </div>
+                <button type='submit' >Confirm</button>
+            </form>
+            <Link to="/Champions/"><button>Back</button></Link>
+        </div>
 
     )
 };
