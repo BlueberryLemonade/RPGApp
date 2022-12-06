7a5805b10b60353e0e968d64409b870511329380
Justin <Justin.Swatloski@gmail.com>
2022-12-06 01:11:33 -0600

    basic unbuilt arena menu

--- Test/dungeon-master/src/components/Arena/ArenaMenu.js
+++ Test/dungeon-master/src/components/Arena/ArenaMenu.js
@@ -5,39 +5,11 @@ import FightPit from './FightPit';
 import { Link } from 'react-router-dom';
 
 const ArenaMenu = props => {
-  const[selectedChampion, setSelectedChampions] = useState("");
-  const[selectedMonster, setSelectedMonster] = useState("");
+ 
+
   const[fightText, setFightText] = useState("All is peaceful currently");
 
-  const champSearch = (name) => {
-    return name.name === selectedChampion;
-  }
 
-  const monsterSearch = (name) => {
-    return name.name === selectedMonster;
-  }
-
-
-  const fightHandler = () => {
-    const champIndex = props.champions.findIndex(champSearch);
-    const enemyIndex = props.monsters.findIndex(monsterSearch);
-
-    const enemy = props.monsters[enemyIndex];
-    const champ = props.champions[champIndex];
-    setFightText(champ.strength + " Damage is done to " + enemy.name + " and " + enemy.strength + " Damage is done to " + champ.name);
-  };
-
-
-  const selectionHandler = (event) => {
-
-    switch(event.target.parentElement.className){
-      case "Champions" : setSelectedChampions(event.target.textContent);
-      break;
-      case "Monsters" : setSelectedMonster(event.target.textContent);
-      break;
-      default :
-    } 
-  }
 
 
 
@@ -45,18 +17,12 @@ const ArenaMenu = props => {
   const arenaOptions = () => {
     return (
       <div>
-       <FightPit selectedMonster={selectedMonster} selectedChampion={selectedChampion} />
 
        <p>{fightText}</p>
 
-        <button onClick={fightHandler}>Fight</button>
-
 
         <div className="monChampContainer">
-          <div className="arenaMonsters">
-            <ArenaList onSelected={selectionHandler} className="Monsters" database={props.monsters} />
-          </div>
-          <ArenaList onSelected={selectionHandler} className="Champions" database={props.champions} />
+          <p>Stuff</p>
         </div>
 
         <Link to="/"><button>Close Arena</button></Link>
