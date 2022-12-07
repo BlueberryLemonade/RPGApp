4ec6b2c53068c881a25ffa556c9a199da106f4d3
Justin <Justin.Swatloski@gmail.com>
2022-12-07 00:42:19 -0600

    functional delete handler

--- Test/dungeon-master/src/components/Champions/Champion.js
+++ Test/dungeon-master/src/components/Champions/Champion.js
@@ -1,5 +1,5 @@
 import './Champion.css';
-//import axios from 'axios';
+import axios from 'axios';
 //import { Link } from 'react-router-dom';
 
 const Champion = (props) => {
@@ -12,7 +12,7 @@ const Champion = (props) => {
         props.onClick(props.id);
     }
 
-/*
+
     const deleteHandler = () => {
         axios.delete("http://localhost:8080/api/champions/" + props.id,)
         .catch(error => {
@@ -22,10 +22,21 @@ const Champion = (props) => {
         props.onChange();
     
     }
-  */  
+  
+ 
+    if(props.selected){
+        return(
+            <div className={ "selectedChamp" } onClick={clickHandler}>
+         <p className="champData">Champion Name: {props.name}    HP: {props.hp}</p>
+         <button className="buttons">Edit</button><button className='buttons' onClick={deleteHandler}>Delete</button>
+    </div>
+         
+ 
+        )
+    }
 
     return(
-        <div className={props.selected ? "selectedChamp" : "champion"} onClick={clickHandler}>
+        <div className="champion" onClick={clickHandler}>
          <p>Champion Name: {props.name}    |    HP: {props.hp}</p>
     </div>
     )
